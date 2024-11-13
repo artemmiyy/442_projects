6faec8ccad9afcf2c1bfe2e51a5568a56b37fc6f
Artemii Polshcha <artempolshcha@gmail.com>
2024-11-12 23:35:26 -0500

    added solution_q2.py

---
+++ frozen_lake_proj/solution_q2.py
@@ -0,0 +1,11 @@
+import gymnasium as gym
+
+env = gym.make('FrozenLake-v1', desc=None, map_name='4x4',is_slippery=True,render_mode='human')
+
+### Q2.2 Code (Random Policy until 1000 episodes)
+
+
+### Q2.3 Code (Value Iteration)
+
+
+### Q2.4 Code (Policy Extraction)
